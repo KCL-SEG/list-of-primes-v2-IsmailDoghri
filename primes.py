1b5f7cff30e74890a776e59a3bf838b4e95d4703
Ismail Doghri <ismail.doghri@kcl.ac.uk>
2022-10-11 17:42:17 +0100

    Primes final solution

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f'Number of primes must be 1 or higher.n/Number specified: {number_of_primes}')
     list = []
+    n = 2
+    prime = True
+    while len(list) < number_of_primes:
+        for i in range(2, n):
+            if n % i == 0:
+                prime = False
+        if prime:
+            list.append(n)
+        n += 1
+        prime = True
+
     return list
